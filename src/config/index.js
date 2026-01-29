d130c12222fc559bcd65f09b7a910ed6b118806b
Kateryna <kunieva.kateryna@gmail.com>
2026-01-29 15:40:34 +0100

    ci: add automated build and push to GHCR with infra trigger

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const config = {
   // Services
-  USERS_SERVICE: 'http://localhost:3000',
+  USERS_SERVICE: process.env.REACT_APP_API_URL || 'http://localhost:8080',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
-  ANIME_SERVICE: 'http://localhost:3000/api/anime',
+  ANIME_SERVICE: `${process.env.REACT_APP_API_URL || 'http://localhost:8080'}/api/anime`,
 };
 
 export default config;
