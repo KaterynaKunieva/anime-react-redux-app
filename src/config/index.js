ecf2dee35702f97b1aee838152bf40cc95ac22c4
Kateryna <kunieva.kateryna@gmail.com>
2026-01-30 21:23:45 +0100

    Removed localhost from config

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const config = {
   // Services
-  USERS_SERVICE: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+  USERS_SERVICE: process.env.REACT_APP_API_URL || '',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
-  ANIME_SERVICE: `${process.env.REACT_APP_API_URL || 'http://localhost:8080'}/api/anime`,
+  ANIME_SERVICE: `${process.env.REACT_APP_API_URL || ''}/api/anime`,
 };
 
 export default config;
