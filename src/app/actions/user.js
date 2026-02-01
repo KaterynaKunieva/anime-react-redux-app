8ce49e055ec9825eaeee51a652ff9e25f73692ce
Kateryna <kunieva.kateryna@gmail.com>
2026-02-01 22:06:02 +0100

    Added logout

--- src/app/actions/user.js
+++ src/app/actions/user.js
@@ -1,5 +1,6 @@
 import axios from 'misc/requests';
 import config from "../../config";
+import storage from '../../misc/storage';
 import {
   RECEIVE_USER,
   REQUEST_SIGN_OUT,
@@ -9,6 +10,11 @@ import {
 const MOCK_USER_RESPONSE = {
   email: 'mock-user@gmail.com',
   name: 'Mock Google User (Offline Mode)',
+};
+
+const MOCK_LOGOUT_RESPONSE = {
+  email: '',
+  name: '',
 };
 
 const receiveUser = (user) => ({
@@ -31,11 +37,15 @@ const getProfile = () => {
   return axios.get(`${USERS_SERVICE}/api/profile`);
 }
 
+const postSignOut = () => {
+  const { USERS_SERVICE } = config;
+  return axios.post(`${USERS_SERVICE}/api/signout`);
+};
+
 const fetchUser = () => (dispatch) => {
   dispatch(requestUser());
   return getProfile()
     .then(data => {
-      console.log("User data received from server:", data);
       dispatch(receiveUser(data));
     })
     .catch((err) => {
@@ -49,6 +59,13 @@ const fetchUser = () => (dispatch) => {
 
 const fetchSignOut = () => (dispatch) => {
   dispatch(requestSignOut());
+  return postSignOut()
+    .catch(() => {
+      dispatch(receiveUser(MOCK_LOGOUT_RESPONSE));
+    })
+    .finally(() => {
+      window.location.href = '/';
+    });
 };
 
 const exportFunctions = {
